e46f5c3c3056966834f146a15fd8ac2ebc8a3c14
Blazebrain <davidadegoke16@gmail.com>
2022-07-10 23:51:15 +0100

    chore: Setup dependencies and codebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:web3dart/web3dart.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Client? httpClient;
+  Web3Client? ethClient;
+
+  final myAddress = '0xF6781AeB9Bfae1fF5768a4219b309954EC787b2C';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [
+          Text('title'),
+        ],
       ),
     );
   }
